aa6a4c7ed2d7bb9ed187387f3ef318ef04031bc7
unknown <koushik.tech2020@gmail.com>
2023-07-16 22:56:18 +0530

    Mongo uri changed

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
+// const mongouri = "mongodb://localhost:27017/foodDB";
+
 const mongouri =
   "mongodb://uev6oqtayvkd12ywvb29:wzt7WchM0NQnsNb6OcM4@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bf4xl5nb5dyze5f?replicaSet=rs0";
 
+//Testing Database connection for local Database
 exports.dbconnect = async () => {
   try {
     await mongoose.connect(mongouri, {
@@ -15,14 +18,3 @@ exports.dbconnect = async () => {
     process.exit();
   }
 };
-
-//Testing Database connection for local Database
-//const db ="mongodb://localhost:27017/foodDB";
-//connecting to Cloud clever Database
-// const db="mongodb://ukpgq4nrdysvvc8izhd7:Kza34Qv5Z8unoXtNDM7N@b9gg12vuhityidl-mongodb.services.clever-cloud.com:27017/b9gg12vuhityidl";
-
-// mongoose.connect(db,(err)=>{
-//     if(err) throw err;
-//     else
-//     console.log('Successfully connected to MongoDB');
-// });
