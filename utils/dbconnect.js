a97a7b219812a2c6fffb61746fe68584817d5734
unknown <koushik.tech2020@gmail.com>
2024-02-03 22:26:58 +0530

    Local mongo url added

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-// const mongouri = "mongodb://localhost:27017/StudenDB";
-
-const mongouri =
-  "mongodb://uev6oqtayvkd12ywvb29:wzt7WchM0NQnsNb6OcM4@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bf4xl5nb5dyze5f?replicaSet=rs0";
+const mongouri = "mongodb://localhost:27017/StudenDB";
 
 //Testing Database connection for local Database
 exports.dbconnect = async () => {
